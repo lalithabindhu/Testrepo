fc468d2c99e9455c6d637739316874b679be4a11
lalithabindhu <98765174+lalithabindhu@users.noreply.github.com>
2022-05-18 13:24:49 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
 
-   version = "~>2.0"
+   # version = "~>2.0"
   
     subscription_id = "7e57765c-ec35-4ba2-beb7-22f355b0bd65"
     tenant_id = "7359f896-71e2-4dae-b8a3-15cdf97f2f10"
